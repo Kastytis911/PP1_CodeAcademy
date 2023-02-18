7c0113b20954a86356289f14d38ab529e8f166cc
Kastytis <zymantas.kastytis@gmail.com>
2023-02-18 14:16:11 +0200

    Sekantis commit'as. Programa scrap'ina du puslapius. Vyriskos striukes ir moteriskos striukes

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import src.scraper as scraper
 
 # Get URL address from config.txt file
 # In config file are some urls. Insert number in read_config.read_url(<number>)
-# 0 - first url
-# 1 - second url
-Selected_url = read_config.read_url(1)
+# 0 - first url is for men jacket
+# 1 - second url is for woman jacket
+Selected_url = read_config.read_url(0)
 
 # Start scraper, using url selected before
-scraper.read_url(Selected_url)
+scraper.scrape_web(Selected_url)
+
 
 
 
--- src/scraper.py
+++ src/scraper.py
@@ -2,10 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def read_url(url):
+def scrape_web(url):
+    # Send an HTTP request to the webpage and get the HTML content
+    response = requests.get(url)
+    html_content = response.content
 
-    resp = requests.get(url, allow_redirects=True)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    print(soup.text)
-    #print(resp.text)
-    #print(resp.status_code)
+    # Parse the HTML content with BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Find all the listings on the page
+    # listings = soup.find_all('div', class_='auto-lists lt')
+    listings = soup.find_all('div', class_='product_element multiple-photos')
+
+    # Loop through the listings and extract the relevant information
+    for listing in listings:
+        # Get the title and price of the listing
+        title = listing.find('span', class_='title').text.strip()
+        price = listing.find('span', class_='price').text.strip()
+
+        # Print the information of the listing
+        print(title)
+        print(price)
